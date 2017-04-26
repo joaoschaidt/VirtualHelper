d8289a3e73a7988748f9ac9017af5a0d9a0ecea2
João Vitor Schaidt <joaoschaidt@hotmail.com>
2017-04-26 14:35:25 -0300

    tabs temporarias

--- app/src/main/java/com/example/joaos/virtualhelper/activity/ContainerEditActivity.java
+++ app/src/main/java/com/example/joaos/virtualhelper/activity/ContainerEditActivity.java
@@ -1,18 +1,15 @@
 package com.example.joaos.virtualhelper.activity;
 
 import android.support.v4.app.DialogFragment;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.ViewSwitcher;
 
-import com.example.joaos.virtualhelper.DatePickerFragment;
+import com.example.joaos.virtualhelper.util.DatePickerFragment;
 import com.example.joaos.virtualhelper.R;
 
 public class ContainerEditActivity extends AppCompatActivity {
